0e715ce978ceb4448e0e3272f7bde0613cf73150
devaraujstein <andreearaujo98@gmail.com>
2020-03-16 16:27:09 -0300

    finalizing gobarber's backend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+import 'dotenv/config';
+
 import express from 'express';
+
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+
 import path from 'path';
+import sentryConfig from './config/sentry';
+
+import 'express-async-errors';
+
 import routes from './routes';
 
 import './database';
@@ -8,12 +18,15 @@ class App {
   constructor() {
     this.server = express();
 
-    this.middlewares();
+    Sentry.init(sentryConfig);
 
+    this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +36,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'developement') {
+        const error = await new Youch(err, res).toJSON();
+
+        return res.status(500).json(error);
+      }
+
+      return res.status(500).json({ error: 'Server Internal Error' });
+    });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,9 +25,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/avaible', AvailableController.index);
 
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
+routes.delete('/appointments/:id', AppointmentsController.delete);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
